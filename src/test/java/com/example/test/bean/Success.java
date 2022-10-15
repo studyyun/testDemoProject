2508d732f275d6ab8960b6d18a833b3952be4a82
zhoushiyun <569824306@qq.com>
2022-10-15 21:07:02 +0800

    提交后台代码

---
+++ src/test/java/com/example/test/bean/Success.java
@@ -0,0 +1,30 @@
+package com.example.test.bean;
+
+/**
+ * 应用模块名称
+ *
+ * @author zhousy
+ * @date 2022-01-13  18:06
+ */
+public class Success {
+    
+    private String msg;
+    
+    private String success;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
